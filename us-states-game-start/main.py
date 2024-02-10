10821cbb40e7f7d5bc4e2ecbfb11d23a4e3fe044
AkinwaleAkinkunmi <akinwaleakinkunmu2005@gmail.com>
2024-02-10 14:59:30 -0800

    Used the iloc method to access the number in the x and y column

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -1,12 +1,12 @@
-import turtle as t
+import turtle
 import pandas as pd
 
-screen = t.Screen()
+screen = turtle.Screen()
 screen.title("U.S States Game")
 image = "blank_states_img.gif"
 
 screen.addshape(image) # Can be used to turn an image to a shape
-t.shape(image)
+turtle.shape(image)
 
 # def get_mouse_click_coor(x, y): # Getting the coordinates of the mouse click on the turtle screen
 #     print(x, y)
@@ -17,16 +17,18 @@ t.shape(image)
 
 playing = True
 
-while playing:
-    answer_states = screen.textinput(title=f"Guess a state", prompt= "What's another state name")
-    answer_states_title = answer_states.title()
-    states_data = pd.read_csv("50_states.csv")
-    states_list = states_data["state"].to_list()
-    
-    if answer_states_title in states_list:
-        t = t.Turtle()
-        t.hideturtle()
-        t.penup()
-        states_data[states_data.state == answer_states]
+answer_states = screen.textinput(title=f"Guess a state", prompt= "What's another state name")
+answer_states_title = answer_states.title()
+states_data = pd.read_csv("50_states.csv")
+states_list = states_data["state"].to_list()
 
+if answer_states_title in states_list:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_row = states_data[states_data.state == answer_states]
+    # t.goto(int(state_row.x), int(state_row.y))
+    t.goto(int(state_row['x'].iloc[0]), int(state_row['y'].iloc[0])) # Gets the item in the x and y column    
+    t.write(answer_states)
 
+screen.exitonclick()
